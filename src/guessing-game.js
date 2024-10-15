937c693647f0f0f2cc7c45877be1b029140c8e03
Webis-2022 <verjovkin.jevgeni@gmail.com>
2024-10-15 09:14:39 +0300

    feat: resolve task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,32 @@
 class GuessingGame {
-    constructor() {}
-
+    constructor() {
+      this.min = null;
+      this.max = null;
+      this.currentGuess = null;
+    }
+  
     setRange(min, max) {
-
+      this.min = min;
+      this.max = max;
     }
-
+  
     guess() {
-
+      if (this.max - this.min === 1) {
+        this.currentGuess = this.max;
+      } else {
+        this.currentGuess = Math.round((this.min + this.max) / 2);
+      }
+      return this.currentGuess;
     }
-
+  
     lower() {
-
+      this.max = this.currentGuess;
     }
-
+  
     greater() {
-
+      this.min = this.currentGuess;
     }
-}
-
+  }
+  
+  
 module.exports = GuessingGame;
